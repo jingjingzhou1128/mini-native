cff6673437ee150b1f4afc1f06d02f8ec520042f
jingjing.zhou1128 <ZJJ_zjj1128@163.com>
2019-12-09 17:59:28 +0800

    add goods detail

--- pages/goodsDetail/index.js
+++ pages/goodsDetail/index.js
@@ -6,7 +6,148 @@ Page({
    */
   data: {
     // 状态栏高度
-    statusBarHeight: 0
+    statusBarHeight: 0,
+    // 顶部导航栏透明度
+    navbarOpacity: 0,
+    // 当前所在滑块index
+    swiperIndex: 1,
+    // 滑块项
+    swiper: [
+      {
+        src: '../../assets/images/fruit.png'
+      },
+      {
+        src: '../../assets/images/fruit.png'
+      },
+      {
+        src: '../../assets/images/fruit.png'
+      }
+    ],
+    // 推荐列表
+    recomList: [
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      },
+      {
+        src: '../../assets/images/fruit.png',
+        title: '湾仔码头三鲜水饺300g',
+        desc: '瞧这一个个白小胖 可爱诱人',
+        price: 33.9
+      }
+    ],
+    // 商品分类移动进度
+    moveProgress: 0
   },
 
   /**
@@ -21,7 +162,7 @@ Page({
    * Lifecycle function--Called when page is initially rendered
    */
   onReady: function () {
-
+    this.getMoveProgress(0)
   },
 
   /**
@@ -64,5 +205,61 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+  
+  /**
+   * @description 监听页面滚动事件
+   */
+  onPageScroll: function (obj) {
+    let opacity
+    if (obj.scrollTop >= 400) {
+      opacity = 1
+    } else {
+      opacity = obj.scrollTop / 400
+    }
+    this.setData({
+      navbarOpacity: opacity
+    })
+  },
+  // swiper 滑动事件处理
+  handlerSwiper: function (e) {
+    this.setData({
+      swiperIndex: e.detail.current + 1
+    })
+  },
+  // move 移动事件处理
+  handlerMove: function (e) {
+    if (this.customData.moveTimer) {
+      clearTimeout(this.customData.moveTimer)
+    }
+    let timer = null
+    this.customData.moveTimer = setTimeout(() => {
+      this.getMoveProgress(e.detail.x)
+    }, 500)
+  },
+  // 获取移动进度
+  getMoveProgress: function (offsetX) {
+    let query = this.createSelectorQuery()
+    let _this = this
+    query.select('#recomMoveWrap').boundingClientRect(rect => {
+      let wrapWidth = rect.width
+      let moveWidth = 130 * _this.data.recomList.length - 5
+      let moveProgress = 0
+      if (moveWidth <= 0) {
+        moveProgress = 0
+      } else if (offsetX === 0) {
+        moveProgress = Math.round((wrapWidth / moveWidth) * 100)
+      } else {
+        moveProgress = Math.round(((-offsetX + wrapWidth) / moveWidth) * 100)
+      }
+      _this.setData({
+        moveProgress: moveProgress
+      })
+    }).exec()
+  },
+  // 自由数据
+  customData: {
+    // 商品分类移动定时器索引
+    moveTimer: null
   }
 })
